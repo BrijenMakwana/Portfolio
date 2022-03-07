d1dedd850dde00fd5275bcc301c12c9fe15863c0
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-07 10:43:03 +0530

    achievements order by appiled

--- src/components/achievementList/AchievementList.jsx
+++ src/components/achievementList/AchievementList.jsx
@@ -7,7 +7,7 @@ export default function AchievementList(props) {
 
     const getAchievements = () => {
         fetch(
-          "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22achievements%22%5D%7B%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20description%2C%0A%20%20url%0A%7D"
+          "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22achievements%22%5D%20%7C%20order(_createdAt%20asc)%7B%0A%20%20_createdAt%2C%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20description%2C%0A%20%20url%0A%7D"
         )
           .then((res) => res.json())
           .then((res) => {
