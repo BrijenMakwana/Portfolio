bb5959057f827e1b9e5681f809335e2881736ef6
Brijen Makwana <brijenmakwana@192.168.1.7>
2023-02-12 16:53:51 +0530

    intro

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,46 +1,42 @@
-import React from 'react'
-import "./Introduction.css"
-import Me from "../../img/brijen.png"
+import React from "react";
+import "./Introduction.css";
+import Me from "../../img/brijen.png";
 
 const Introduction = (props) => {
-
   return (
-    <div className='i'>
-        <div className='i-left'>
-            <div className='i-left-wrapper'>
-                <h2 className='i-intro'>Hello, My Name is</h2>
-                <h1 className='i-name'>{props.name}</h1>
-                <div className='i-title'>
-                    <div className='i-title-wrapper'>
-                        {
-                            props.position && props.position.split(",").map((item,index)=>(
-                                <div key={index} className='i-title-item' style={{color: props.randomColor}}>{item}</div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div className='i-desc'>
-                    <b>Hello There! ✌🏻</b><br/>
-                    <ul>
-                        {
-                            props.introduction && props.introduction.split(",").map((item,index)=>(
-                                <li key={index}>{item}</li>
-                            ))
-                        }
-                        
-                    </ul>
-                </div>
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My Name is</h2>
+          <h1 className="i-name">{props.name}</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              {props.position &&
+                props.position.split(",").map((item, index) => (
+                  <div key={index} className="i-title-item">
+                    {item}
+                  </div>
+                ))}
             </div>
+          </div>
+          <div className="i-desc">
+            <b>Hello There! ✌🏻</b>
+            <br />
+            <ul>
+              {props.introduction &&
+                props.introduction
+                  .split(",")
+                  .map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+          </div>
         </div>
-        <div className='i-right'>
-            <div className='i-bg' style={{backgroundColor: props.randomColor}}></div>
-            <img className='i-img'
-                src={Me} 
-                alt='Brijen Makwana'
-            />
-        </div>
+      </div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img className="i-img" src={Me} alt="Brijen Makwana" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Introduction
+export default Introduction;
