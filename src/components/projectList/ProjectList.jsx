ce73016315f491d955a899c1acdcc0df26f47e17
Brijen Makwana <brijenmakwana@192.168.1.4>
2023-03-06 12:08:48 +0530

    navbar added

--- src/components/projectList/ProjectList.jsx
+++ src/components/projectList/ProjectList.jsx
@@ -1,45 +1,42 @@
-import React,{useState,useEffect} from 'react'
-import "./ProjectList.css"
-import ProjectItem from "../projectItem/ProjectItem" 
+import React, { useState, useEffect } from "react";
+import "./ProjectList.css";
+import ProjectItem from "../projectItem/ProjectItem";
 
 export default function ProjectList(props) {
-  const [projects,setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-    const getProjects = () => {
-        fetch(
-          "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22projects%22%5D%7B%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20description%2C%0A%20%20url%0A%7D"
-        )
-          .then((res) => res.json())
-          .then((res) => {
-              
-              setProjects(res.result);
-          });
-      };
-    
-      useEffect(() => {
-        getProjects();
-      }, []);
-  
+  const getProjects = () => {
+    fetch(
+      "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22projects%22%5D%7B%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%2C%0A%20%20description%2C%0A%20%20url%0A%7D"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setProjects(res.result);
+      });
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
-    <div className='pl'>
-        <div className='pl-texts'>
-            <h1 className='pl-title'>{props.heading}</h1>
-            <p className='pl-desc'>
-            {props.description}
-            </p>
-        </div>
-        <div className='pl-list'>
-        {
-            projects && projects.map((item,index)=>(
-                <ProjectItem 
-                    key={index} 
-                    title={item.title} 
-                    imageUrl={item.imageUrl} 
-                    url={item.url} 
-                    description={item.description}/>
-            ))
-        }
-        </div>
+    <div className="pl" id="projects">
+      <div className="pl-texts">
+        <h1 className="pl-title">{props.heading}</h1>
+        <p className="pl-desc">{props.description}</p>
+      </div>
+      <div className="pl-list">
+        {projects &&
+          projects.map((item, index) => (
+            <ProjectItem
+              key={index}
+              title={item.title}
+              imageUrl={item.imageUrl}
+              url={item.url}
+              description={item.description}
+            />
+          ))}
+      </div>
     </div>
-  )
+  );
 }
--- src/components/skillList/SkillList.jsx
+++ src/components/skillList/SkillList.jsx
@@ -1,37 +1,39 @@
-import React,{useState,useEffect} from 'react'
-import "./SkillList.css"
-import SkillItem from '../skillItem/SkillItem';
+import React, { useState, useEffect } from "react";
+import "./SkillList.css";
+import SkillItem from "../skillItem/SkillItem";
 
 export default function SkillList(props) {
-    const [skills,setSkills] = useState([]);
+  const [skills, setSkills] = useState([]);
 
-    const getAllSkills = () => {
-         fetch(
-            "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22skills%22%5D%20%7C%20order(_createdAt%20asc)%7B%0A%20%20_createdAt%2C%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%0A%7D"
-          )
-            .then((res) => res.json())
-            .then((res) => {
-                
-                setSkills(res.result);
-            });
-    }
+  const getAllSkills = () => {
+    fetch(
+      "https://gzsq4ssh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22skills%22%5D%20%7C%20order(_createdAt%20asc)%7B%0A%20%20_createdAt%2C%0A%20%20title%2C%0A%20%20%22imageUrl%22%3A%20image.asset-%3Eurl%0A%7D"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setSkills(res.result);
+      });
+  };
 
-    useEffect(()=>{
-        getAllSkills();
-    },[])
+  useEffect(() => {
+    getAllSkills();
+  }, []);
 
   return (
-    <div className='sl'>
-    <div className='sl-texts'>
-        <h1 className='sl-title'>{props.heading}</h1>
+    <div className="sl" id="skills">
+      <div className="sl-texts">
+        <h1 className="sl-title">{props.heading}</h1>
+      </div>
+      <div className="sl-list">
+        {skills &&
+          skills.map((item, index) => (
+            <SkillItem
+              key={index}
+              title={item.title}
+              imageUrl={item.imageUrl}
+            />
+          ))}
+      </div>
     </div>
-    <div className='sl-list'>
-        {
-            skills && skills.map((item,index)=>(
-                <SkillItem key={index} title={item.title} imageUrl={item.imageUrl}/>
-            ))
-        }
-    </div>
-</div>
-  )
+  );
 }
