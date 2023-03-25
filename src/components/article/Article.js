b9eabbfcc553fa75ca15959b421864e2c51ff7b9
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-03-25 17:13:58 +0530

    articles

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -3,13 +3,15 @@ import "./Article.css";
 import moment from "moment";
 
 export const Article = (props) => {
-  const { title, brief, coverImage, dateAdded } = props;
+  const { title, brief, coverImage, dateAdded, slug } = props;
   return (
     <div className="article-container">
-      <span className="date">{moment(dateAdded).format("LL")}</span>
-      <img src={coverImage} alt={title} className="article-image" />
-      <h2 className="article-title">{title}</h2>
-      <p className="article-description">{brief}</p>
+      <a href={`https://brijen.hashnode.dev/${slug}`} target="blank">
+        <span className="date">{moment(dateAdded).format("LL")}</span>
+        <img src={coverImage} alt={title} className="article-image" />
+        <h2 className="article-title">{title}</h2>
+        <p className="article-description">{brief}</p>
+      </a>
     </div>
   );
 };
--- src/components/articleList/ArticleList.js
+++ src/components/articleList/ArticleList.js
@@ -21,6 +21,7 @@ export const ArticleList = () => {
                 dateAdded
                 brief
                 coverImage
+                slug
               }
             }
           }
