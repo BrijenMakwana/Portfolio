9f6174afe903b91ea580ea6f2643daf962535085
Brijen Makwana <brijenma@gmail.com>
2023-10-19 16:08:10 +0530

    code refactor

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,11 +4,12 @@ export default function Footer(props) {
   return (
     <footer className="f" id="contact">
       <div className="f-texts">
-        <h1 className="f-title">Contact Details</h1>
-        <p className="f-desc">
+        <h2 className="f-title">Contact Details</h2>
+
+        <>
           <a href={`mailto:${props.email}`}>{props.email}</a> <br />
           <a href={`tel:${props.phone}`}>{props.phone}</a>
-        </p>
+        </>
       </div>
     </footer>
   );
--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -4,7 +4,9 @@ export default function Skill(props) {
   const { title, image } = props;
   return (
     <div className="s">
-      <img src={image} alt={title} className="s-img" />
+      <div className="img-container">
+        <img src={image} alt={title} className="s-img" />
+      </div>
 
       <h2 className="s-title">{title}</h2>
     </div>
